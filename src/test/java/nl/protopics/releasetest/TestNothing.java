c6c9726b06ac98c7354f1d94b8f87fc91b41b322
Floris Kruisselbrink <floris@vloris.nl>
2015-01-27 18:44:56 +0100

    Unit-test toegevoegd (die niets doet)

---
+++ src/test/java/nl/protopics/releasetest/TestNothing.java
@@ -0,0 +1,14 @@
+package nl.protopics.releasetest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestNothing
+{
+	@Test
+	public void testNothing()
+	{
+		System.err.println("Test Nothing");
+	}
+}

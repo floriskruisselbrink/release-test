86e17017bb3933ff43e55aba4e90632dbb031661
Floris Kruisselbrink <floris@vloris.nl>
2015-01-28 15:58:38 +0100

    common functies ge-extraheerd naar eigen file

--- etc/jenkins-post-build.sh
+++ etc/jenkins-post-build.sh
@@ -5,44 +5,15 @@
 # MVN_RELEASE_VERSION=0.4
 # MVN_DEV_VERSION=0.5-SNAPSHOT
 
+DIR=$(cd "$(dirname "$0")" && pwd)
+source $DIR/jenkins-functions.sh
+
 if [[ ${IS_M2RELEASEBUILD} == true && ${MVN_ISDRYRUN} == false ]] ; then
-	REPO=$(git config --get remote.origin.url)
-	REPO_PATH=${REPO##https://}
-	PUSH_REPO="https://${USERNAME}@${REPO_PATH}"
+	init_variables
+	init_credential_helper
 
-	# Volgende blok code bevat password van de gebruiker, dit niet in jenkins loggen!
-	set +x
-	HOSTNAME=${REPO_PATH%%/*}
-	CREDENTIALS="https://${USERNAME}:${PASSWORD}@${HOSTNAME}"
+	finish_release
+	push_changes
 
-	CREDENTIAL_FILE=${WORKSPACE}/target/.git-credentials
-
-cat <<EOF > ${CREDENTIAL_FILE}
-${CREDENTIALS}
-EOF
-	set -x
-
-	git config credential.helper "store --file ${CREDENTIAL_FILE}"
-
-	START_BRANCH=${GIT_BRANCH##origin/}
-	RELEASE_BRANCH=release/${MVN_RELEASE_VERSION}
-	MASTER_BRANCH=master
-
-	git checkout ${MASTER_BRANCH}
-	git merge --no-ff ${RELEASE_BRANCH}
-	git tag -m "Versie ${MVN_RELEASE_VERSION}" v${MVN_RELEASE_VERSION}
-
-	git checkout ${START_BRANCH}
-	git merge --no-ff ${RELEASE_BRANCH}
-	mvn versions:set -DnewVersion=${MVN_DEV_VERSION} versions:commit
-	git commit -a -m "Verder op ${MVN_DEV_VERSION}"
-
-	git push ${PUSH_REPO} refs/heads/${MASTER_BRANCH}:refs/heads/${MASTER_BRANCH}
-	git push ${PUSH_REPO} refs/tags/v${MVN_RELEASE_VERSION}
-	git push ${PUSH_REPO} refs/heads/${START_BRANCH}:refs/heads/${START_BRANCH}
-
-	git branch -d ${RELEASE_BRANCH}
-
-	rm ${CREDENTIAL_FILE}
-	git config --unset credential.helper
+	cleanup_credential_helper
 fi
--- etc/jenkins-pre-build.sh
+++ etc/jenkins-pre-build.sh
@@ -5,12 +5,11 @@
 # MVN_RELEASE_VERSION=0.4
 # MVN_DEV_VERSION=0.5-SNAPSHOT
 
-if [[ ${IS_M2RELEASEBUILD} == true && ${MVN_ISDRYRUN} == false ]] ; then
-	START_BRANCH=${GIT_BRANCH##origin/}
-	RELEASE_BRANCH=release/${MVN_RELEASE_VERSION}
+DIR=$(cd "$(dirname "$0")" && pwd)
+source $DIR/jenkins-functions.sh
 
-	git checkout ${START_BRANCH}
-	git checkout -b ${RELEASE_BRANCH}
-	mvn versions:set -DnewVersion=${MVN_RELEASE_VERSION} versions:commit
-	git commit -a -m "Versie ${MVN_RELEASE_VERSION}"
+if [[ ${IS_M2RELEASEBUILD} == true && ${MVN_ISDRYRUN} == false ]] ; then
+	init_variables
+
+	start_release
 fi

3a72dfdce9139067dbb243b96bfb1372f8420734
Floris Kruisselbrink <floris@vloris.nl>
2015-01-28 12:15:48 +0100

    jenkins-post-build script: username/password expliciet aangeven bij push

--- etc/jenkins-post-build.sh
+++ etc/jenkins-post-build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+REPO="https://${USERNAME}:${PASSWORD}@github.com/vloris/release-test.git"
 CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
 MASTER_BRANCH=master
 
 git commit -a -m "Versie ${RELEASE_VERSION}"
-git push origin ${CURRENT_BRANCH}
+git push --repo ${REPO} origin ${CURRENT_BRANCH}
 
 git checkout ${MASTER_BRANCH}
 git merge --no-ff ${CURRENT_BRANCH}
 git tag -m "Versie ${RELEASE_VERSION}" v${RELEASE_VERSION}
-git push --tags origin ${MASTER_BRANCH}
+git push --repo ${REPO} --tags origin ${MASTER_BRANCH}
 
 git checkout ${CURRENT_BRANCH}
 mvn versions:set -DnewVersion=${NEXT_VERSION} versions:commit
 git commit -a -m "Verder op ${NEXT_VERSION}"
-git push origin ${CURRENT_BRANCH}
+git push --repo ${REPO} origin ${CURRENT_BRANCH}

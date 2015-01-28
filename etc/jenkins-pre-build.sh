353fb376498f2f9bdd7c53c79361bec062a1dbda
Floris Kruisselbrink <floris@vloris.nl>
2015-01-28 13:35:52 +0100

    Fix pre-buildscript: branch uitchecken

--- etc/jenkins-pre-build.sh
+++ etc/jenkins-pre-build.sh
@@ -6,9 +6,10 @@
 # MVN_DEV_VERSION=0.5-SNAPSHOT
 
 if [[ ${IS_M2RELEASEBUILD} == true && ${MVN_ISDRYRUN} == false ]] ; then
-	START_BRANCH=$(git symbolic-ref --short -q HEAD)
+	START_BRANCH=${GIT_BRANCH##origin/}
 	RELEASE_BRANCH=release/${MVN_RELEASE_VERSION}
 
+	git checkout ${START_BRANCH}
 	git checkout -b ${RELEASE_BRANCH}
 	mvn versions:set -DnewVersion=${MVN_RELEASE_VERSION} versions:commit
 	git commit -a -m "Versie ${MVN_RELEASE_VERSION}"

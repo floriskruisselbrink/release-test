6d07446b7225e84dc7c6ec4c98fd9b866efd9d40
Floris Kruisselbrink <floris@vloris.nl>
2015-01-28 13:04:24 +0100

    pre-build script toegevoegd, release-branch aanmaken/verwijderen toegevoegd

--- etc/jenkins-post-build.sh
+++ etc/jenkins-post-build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 REPO="https://${USERNAME}:${PASSWORD}@github.com/vloris/release-test.git"
-CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
+RELEASE_BRANCH=release/${RELEASE_VERSION}
 MASTER_BRANCH=master
 
-git commit -a -m "Versie ${RELEASE_VERSION}"
-
 git checkout ${MASTER_BRANCH}
-git merge --no-ff ${CURRENT_BRANCH}
+git merge --no-ff ${RELEASE_BRANCH}
 git tag -m "Versie ${RELEASE_VERSION}" v${RELEASE_VERSION}
 git push ${REPO} refs/heads/${MASTER_BRANCH}:refs/heads/${MASTER_BRANCH}
 git push ${REPO} refs/tags/v${RELEASE_VERSION}
 
-git checkout ${CURRENT_BRANCH}
+git checkout ${START_BRANCH}
+git merge --no-ff ${RELEASE_BRANCH}
 mvn versions:set -DnewVersion=${NEXT_VERSION} versions:commit
 git commit -a -m "Verder op ${NEXT_VERSION}"
-git push ${REPO} refs/heads/${CURRENT_BRANCH}:refs/heads/${CURRENT_BRANCH}
+
+git push ${REPO} refs/heads/${START_BRANCH}:refs/heads/${START_BRANCH}
+
+git branch -d ${RELEASE_BRANCH}
